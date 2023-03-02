6a7ef6ae1400058286bb54e93803edbe6bf9aa9c
luvero <luciana_lr@hotmail.com>
2023-03-01 23:18:27 -0300

    cv

---
+++ main.js
@@ -0,0 +1,20 @@
+let menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu() {
+  if (menu_visible == false) {
+    //si esta oculto
+    menu.style.display = "block";
+    menu_visible = true;
+  } else {
+    menu.style.display = "none";
+    menu_visible = false;
+  }
+}
+
+let links = document.querySelectorAll("nav a");
+for (const x = 0; x < links.length; x++) {
+  links[x].onclick = function () {
+    menu.style.display = "none";
+    menu_visible = false;
+  };
+}
